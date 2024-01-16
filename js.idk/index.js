55722c9b1c44586055f73d2ba4df5a299e661296
gkggkudtu <156801714+gkggkudtu@users.noreply.github.com>
2024-01-16 04:53:12 -0800

    Add files via upload

---
+++ js.idk/index.js
@@ -0,0 +1,12 @@
+document.head.innerHTML = "<style>em {color: rgb(26, 500, 171);}</style>";
+
+//alert("you are about to expeirence awsomnes like never before")
+
+//window.open("https://www.mariowiki.com/Don_Bongo")
+
+//const successCallback = (position) => {
+
+//console.log(position)};
+
+//navigator.geolocation.getCurrentPosition(successCallback)
+console.log(history);
